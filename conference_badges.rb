9c0f39605e8ae492b6202fd1bbbdb457f942e331
Matthew Shin <mshin1995@gmail.com>
2019-02-14 01:11:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,8 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.collect {|name| badge_maker(name)}
+  attendees.collect do {|name| badge_maker(name)}
+  end
 end
 
 def assign_rooms(attendees)
