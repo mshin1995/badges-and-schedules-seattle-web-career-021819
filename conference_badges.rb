0c0395e5932f20b59819c2b298442f433d61dc36
Matthew Shin <mshin1995@gmail.com>
2019-02-14 01:04:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect {|name| badge_maker(name)}
+end
+
+def assign_rooms(attendees)
+  new_array = []
+  attendees.each_with_index {|name, index|}
+    puts "Hello, #{name}! You'll be assigned to room #{index}!"
+    
+end
