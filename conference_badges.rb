1cb225ccfe2b256ef9b5dd7172ab6343df2f18f9
Matthew Shin <mshin1995@gmail.com>
2019-02-14 01:10:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,13 +8,13 @@ end
 
 def assign_rooms(attendees)
   new_array = []
-  attendees.each_with_index do |name, index|
+  attendees.each_with_index |name, index|
     new_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
   return new_array
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees)
+  batch_badge_creator(attendees).each
   
 end
